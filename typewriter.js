2d354147f89693db5cf3e439c31b72493f55deba
sahasand <130415370+sahasand@users.noreply.github.com>
2023-04-24 10:31:34 -0400

    Add files via upload

---
+++ typewriter.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typewriter = document.querySelector(".typewriter");
+  typewriter.style.width = "0";
+  typewriter.style.animation = "typing 4s steps(40, end), blink-caret .75s step-end infinite";
+});
